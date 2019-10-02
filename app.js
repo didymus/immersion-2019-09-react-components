cf062dd2e647143805f6b6aa54e49beb3325809f
thomasmudge <alephcult@gmail.com>
2019-10-02 12:07:08 -0500

    Refactored GroceryListItem to be class component

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // TODO
-const GroceryList = (props) => {
-const onListItemClick = (event) => {
-    console.log('I got clicked');
-};
-
+class GroceryListItem extends React.Component {
+// a `constructor` method is expected on all ES6 classes
+// when React instantiates the component,
+// it ill pass `props` to the contructor
+constructor(props){
+    // equivalent to ES5's React.Component.call(this, props)
+    super(props);
+}
+// every class component must have a `render` method
+// stateless functional components are pretty much just this method
+render(){
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
 return (
+        <li>{this.props.item}</li>
+);
+}
+}
+
+// upate our `GroceryList` to use the new `GroceryListItem` component
+// this can still be a stateless function component!
+
+const GroceryList = (props) => (
     <ul>
-        <li onClick={onListItemClick}>{props.items[0]}</li>
-        <li>{props.items[1]}</li>
-        <li>{props.items[2]}</li>
+        {props.items.map(item => <GroceryListItem item={item} />)}
     </ul>
 );
-};
 
-const App = () => (
-    <div>
-        <h2>Grocery List</h2>
-        {/* Here we are creating an instance of GroceryList component
-        */}
-        <GroceryList items={['Cat food', 'Cat food', 'Cat food'
-    ]}/>
-    </div>
-);
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
