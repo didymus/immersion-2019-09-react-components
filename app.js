a220335cd372fd53b40c13ef45e5eac024cc9a3b
thomasmudge <alephcult@gmail.com>
2019-10-02 11:55:09 -0500

    Created GroceryList clickHandler

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 // TODO
-// [X] Inside app.js, create a GroceryList component 
-// that contains an unordered list of 2 grocery 
-// items.Render this component to the div tag in 
-// index.html with an id of app
+const GroceryList = (props) => {
+const onListItemClick = (event) => {
+    console.log('I got clicked');
+};
 
-// [X] To appreciate what ES6 and JSX offers when 
-// writing React code, copy the snippets above into 
-// the Babel REPL and see what they look like in ES5
-const GroceryList = (props) => (
+return (
     <ul>
-        <li>{props.items[0]}</li>
+        <li onClick={onListItemClick}>{props.items[0]}</li>
         <li>{props.items[1]}</li>
         <li>{props.items[2]}</li>
     </ul>
 );
+};
 
 const App = () => (
     <div>
